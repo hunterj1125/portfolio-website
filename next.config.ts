276ef2ebe41bbb126358569c77fd50e8efe63d01
Joshua Wehunt <joshuawehunt2@gmail.com>
2026-01-08 23:58:27 -0500

    Rebuild with correct basePath and assetPrefix

--- next.config.ts
+++ next.config.ts
@@ -3,10 +3,11 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: 'export',
   basePath: '/portfolio-website',
-  assetPrefix: '/portfolio-website',
+  assetPrefix: '/portfolio-website/',
   images: {
     unoptimized: true,
   },
 };
 
 export default nextConfig;
+
