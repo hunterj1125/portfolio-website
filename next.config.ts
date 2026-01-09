5a12b2b4a484ac30506574c0e63ce2fc1b24e760
Joshua Wehunt <joshuawehunt2@gmail.com>
2026-01-09 00:14:23 -0500

    Remove basePath - deploy to root domain

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export',
-  basePath: '/portfolio-website',
-  assetPrefix: '/portfolio-website/',
+  output: "export",
   images: {
     unoptimized: true,
   },
 };
 
 export default nextConfig;
-
